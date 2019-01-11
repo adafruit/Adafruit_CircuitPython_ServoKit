63a58a8b88b73b464b5af8c63470fcc6caf1c031
Kattni Rembor <kattni@adafruit.com>
2019-01-11 13:07:17 -0500

    Adding multi-servo examples

---
+++ examples/servokit_all_servos_synchronised.py
@@ -0,0 +1,16 @@
+"""Example that rotates servos on every channel to 180 and then back to 0."""
+import time
+from adafruit_servokit import ServoKit
+
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+kit = ServoKit(channels=8)
+
+# Change range to the number of servos connected.
+# They must be on channels within the specified range!
+for i in range(8):
+    kit.servo[i].angle = 180
+time.sleep(1)
+for i in range(8):
+    kit.servo[i].angle = 0
+time.sleep(1)
