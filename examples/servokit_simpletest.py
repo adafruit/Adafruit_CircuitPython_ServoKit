df755158de861f7cb0a421a8e59b48a2bffe8e4c
foamyguy <foamyguy@gmail.com>
2025-05-16 19:08:06 +0000

    change to ruff

--- examples/servokit_simpletest.py
+++ examples/servokit_simpletest.py
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: MIT
 
 """Simple test for a standard servo on channel 0 and a continuous rotation servo on channel 1."""
+
 import time
+
 from adafruit_servokit import ServoKit
 
 # Set channels to the number of servo channels on your kit.
